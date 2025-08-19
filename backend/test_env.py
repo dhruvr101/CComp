e4c2367f9fe02225f9e5f3c56d286587d8ed5bcc
dhruvr101 <dhruvreddy05@gmail.com>
2025-08-19 01:08:31 -0700

    hi

--- backend/test_env.py
+++
@@ -1,7 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-# Force-load the .env file
-load_dotenv(dotenv_path=os.path.join(os.path.dirname(__file__), ".env"))
-
-print("GOOGLE_APPLICATION_CREDENTIALS =", os.getenv("GOOGLE_APPLICATION_CREDENTIALS"))
